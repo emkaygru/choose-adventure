7966584f98877122c2d28e66bc2d83b883efcdeb
Em <emilylauder@gmail.com>
2021-03-03 21:51:12 -0500

    updated Server File

--- server.js
+++ server.js
@@ -1,36 +1,34 @@
-const express = require('express');
-const mongoose = require('mongoose');
+const express = require("express");
+const mongoose = require("mongoose");
 const app = express();
-const cookieParser = require('cookie-parser')
+const cookieParser = require("cookie-parser");
 const PORT = process.env.PORT || 5000;
-
 
 app.use(cookieParser());
 app.use(express.json());
 
 const db = require("./config/keys").mongoURI;
 
-const url = 'mongodb+srv://emilyemily:Project3oof@cluster0.cmkyl.mongodb.net/choose-adventure?retryWrites=true&w=majority';
-
-mongoose.connect(url,
+mongoose.connect(
+  process.env.MONGODB_URI || "mongodb://localhost/choose-adventure",
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useCreateIndex: true,
-    useFindAndModify: false
+    useFindAndModify: false,
   }
 );
 
-if (process.env.NODE_ENV === 'production') {
-    app.use(express.static('client/build'));
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
 }
 
-const userRouter = require('./routes/User');
-app.use('/user',userRouter);
+const userRouter = require("./routes/User");
+app.use("/user", userRouter);
 
-const storySectionRouter = require('./routes/api/storySection')
-app.use('/story', storySectionRouter)
+const storySectionRouter = require("./routes/api/storySection");
+app.use("/story", storySectionRouter);
 
-app.listen(PORT, function() {
-    console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
+app.listen(PORT, function () {
+  console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
 });
